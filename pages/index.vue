4540e00a00799891b1716cc1c4743a6d3f2b66ed
blackinitial <alhanif.blackinitial@gmail.com>
2021-04-14 11:32:01 +0700

    add surah page, add quran img

--- pages/index.vue
+++ pages/index.vue
@@ -13,19 +13,26 @@
           <div class="title">Al Fatihah</div>
           <div class="subtitle">Ayat 6</div>
         </div>
-        <svg class="bg-icon" width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M79.1667 8.375H25C19.975 8.375 12.5 11.7042 12.5 20.875V79.2083C12.5 88.3792 19.975 91.7083 25 91.7083H87.5V83.375H25.05C23.125 83.325 20.8333 82.5625 20.8333 79.2083C20.8333 78.7875 20.8708 78.4125 20.9333 78.0708C21.4 75.675 23.3625 75.0833 25.0458 75.0417H83.3333C83.4083 75.0417 83.4625 75.0042 83.5375 75H87.5V16.7083C87.5 12.1125 83.7625 8.375 79.1667 8.375ZM79.1667 66.7083H20.8333V20.875C20.8333 17.5167 23.125 16.7583 25 16.7083H54.1667V45.875L62.5 41.7083L70.8333 45.875V16.7083H79.1667V66.7083Z" fill="white"/>
-        </svg>
+        <img class="bg" src="/quran.webp" alt="al qur'an">
       </div>
 
       <h4>Daftar Surat</h4>
 
+      <!-- loading skeleton -->
+      <van-skeleton 
+        v-for="n in 5" :key="n" avatar
+        title :row="1" row-width="100%"
+        style="margin-bottom: 16px"
+        :loading="loading" />
+
+      <!-- list surah -->
       <van-cell-group>
         <van-cell 
           v-for="(surah, index) in surahInfoArray"
           :key="`surah-${index}`" size="large"
           :title="surah.latin" :value="surah.arabic" value-class="surah-arabic"
-          :label="`${surah.translation} - ${surah.ayah_count} Ayat`" center>
+          :label="`${surah.translation} • ${surah.ayah_count} Ayat`" center
+          :to="getSurahDetailUrl(surah, index)">
           <template #icon>
             <div class="ayah-number">
               <span class="number">{{ index + 1 }}</span>
@@ -49,11 +56,13 @@
 import Logo from '~/components/Logo.vue'
 import { ApiPath } from '../constant'
 export default {
+  name: 'Home',
   components: {
     Logo
   },
   data: () => ({
-    surahInfoArray: []
+    surahInfoArray: [],
+    loading: true
   }),
 
   mounted() {
@@ -61,14 +70,19 @@ export default {
   },
 
   methods: {
+    getSurahDetailUrl(surah, index) {
+      return `/${index+1}`
+    },
+
     fetchSurahInfo() {
       fetch(ApiPath.SURAH_INFO)
         .then(response => {
-          console.log('oy')
+          console.log('get surah datas')
           return response.json()
         })
         .then(data => {
           this.surahInfoArray = data.surah_info
+          this.loading = false
         })
     }
   }
@@ -86,8 +100,10 @@ export default {
   flex-direction: column;
   justify-content: space-between;
   align-content: space-between;
-  height: 80px;
+  height: 100px;
   overflow: hidden;
+  animation: 1s appear;
+  z-index: 1;
 }
 .home-slide .title {
   font-weight: bold;
@@ -95,11 +111,14 @@ export default {
 .home-slide .subtitle {
   opacity: 0.8;
 }
-.home-slide .bg-icon {
+.home-slide .bg {
   position: absolute;
-  bottom: 0;
+  bottom: -15%;
   right: 0;
-  transform: rotate(20deg) scale(1.2) translateX(10px);
+  transform: scale(.8) translateX(10%) rotate(25deg);
+  opacity: .75;
+  z-index: -1;
+  pointer-events: none;
 }
 .ayah-number {
   display: flex;
@@ -118,5 +137,10 @@ export default {
   color: #8855CC;
   font-size: 18px;
   font-weight: bold;
+}
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
 }
 </style>
